e191e779ef24911b1523ae6a4ba3ae10402ca361
puvanakopis <puvanakopis@gmail.com>
2025-05-12 15:59:43 +0530

    adding Why Choose Us in Home Page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Hero from "../Components/Home/Hero"
 import PopularRooms from '../Components/Home/PopularRooms'
 import Heading from '../Components/Home/Heading'
+import WhyChooseUs from '../Components/Home/WhyChooseUs'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <Heading title= "Our Popular Rooms"/>
-      <PopularRooms/>
+      <Hero />
+      <Heading title="Our Popular Rooms" />
+      <PopularRooms />
+      <Heading title="Why Choose Us?" />
+      <WhyChooseUs />
+      <Heading title="Our Popular Transport Options" />
     </div>
   )
 }
