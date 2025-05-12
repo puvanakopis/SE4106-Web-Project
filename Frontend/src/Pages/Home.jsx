d0e8c0282fd81c113e1c50cab04866b2f84a421a
puvanakopis <puvanakopis@gmail.com>
2025-05-12 15:29:17 +0530

    adding Popular Rooms in Home Page

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from "../Components/Home/Hero"
+import PopularRooms from '../Components/Home/PopularRooms'
+import Heading from '../Components/Home/Heading'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Heading title= "Our Popular Rooms"/>
+      <PopularRooms/>
     </div>
   )
 }
