df70e3c9887ab4f74331474c6e3d3d8736fcef0f
puvanakopis <puvanakopis@gmail.com>
2025-05-13 12:26:12 +0530

    adding login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home"
 import Footer from './Components/Footer/Footer';
+import Login from './Pages/LogIn';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </>
