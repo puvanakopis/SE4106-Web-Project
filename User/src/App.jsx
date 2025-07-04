ce63a79af87f150afd0512840298ae315803a0c8
puvanakopis <puvanakopis@gmail.com>
2025-07-05 01:03:47 +0530

    Added authentication context and protected routes

--- User/src/App.jsx
+++ User/src/App.jsx
@@ -13,8 +13,6 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Transport from "./Pages/Transport";
 import TransportDetails from "./Pages/TransportDetails";
-
-import MyBookings from "./Pages/MyBookings";
 import Accommodation from "./Pages/Accommodation";
 import RoomDetails from "./Pages/RoomDetails";
 import ForgotPassword from "./Pages/ForgotPassword";
@@ -23,8 +21,7 @@ import NotFound from "./Pages/NotFound";
 
 import { AuthProvider } from "./Context/AuthContext";
 import { BookingProvider } from "./Context/BookingContext";
-
-
+import ProtectedRoute from "./Context/ProtectedRoute";
 
 function App() {
   return (
@@ -33,36 +30,27 @@ function App() {
         <Navbar />
 
         <Routes>
+          {/* Public Routes */}
           <Route path="/" element={<Home />} />
           <Route path="/transport" element={<Transport />} />
           <Route path="/transport/:id" element={<TransportDetails />} />
-
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
-
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/saved" element={<Saved />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-
-
           <Route path="/accommodation" element={<Accommodation />} />
           <Route path="/room/:id" element={<RoomDetails />} />
 
+          {/* Protected Routes */}
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />       
+          <Route path="/saved" element={<ProtectedRoute><Saved /></ProtectedRoute>} />
+          <Route path="/booking" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
 
-
-
-          <Route path="/transport" element={<Transport />} />
-          <Route path="/booking" element={<Booking />} />
-
-          <Route path="/my-bookings" element={<MyBookings />} />
-
-           <Route path="*" element={<NotFound />} /> 
-
+          {/* 404 Not Found */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
+
         <Footer />
       </BookingProvider>
     </AuthProvider>
